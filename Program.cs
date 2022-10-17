1e6b6fade70e7e88a4c0ce28e9640649e84ac2aa
Abriansyah Adam <abriansyah.adam.ft21@mail.umy.ac.id>
2022-10-17 17:20:23 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿namespace QuickShort
+{
+    internal class Program
+    {
+
+
+        private int[] arr = new int[20];
+        private int cmp_count = 0;
+        private int mov_count = 0;
+
+        private int n;
+
+        void read()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 element \n");
+            }
+
+            Console.WriteLine("\n======================");
+            Console.WriteLine("Enter Array Elements");
+            Console.WriteLine("\n======================");
+
+            for(int i =0;i < n;i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+        }
+
+        void swap(int x, int y)
+        {
+            int temp;
+
+            temp = arr[x];
+            arr[x] = arr[y];
+        }
+
+        public void q_sort(int low, int high)
+        {
+            int pivot, i, j;
+            if(low > high)
+                return;
+
+            //Partition the list into two parts
+            //one containing elements less that or equal to pivot
+            //outher containing 
+
+            i = low + 1;
+            j = high;
+
+            pivot = arr[low];
+
+            while (i <= j)
+            {
+                while ((arr[i] <= pivot) && (i <= high))
+                {
+                    i++;
+                    cmp_count++;
+                }
+                cmp_count++;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
