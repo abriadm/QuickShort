04a5c8e4ff240237cd73a3c095bc2836688b2fea
Abriansyah Adam <abriansyah.adam.ft21@mail.umy.ac.id>
2022-10-17 17:35:12 +0700

    changes

--- Program.cs
+++ Program.cs
@@ -71,8 +71,16 @@
                 {
                     //swap the element at index i with the element at index j
                     swap(i, j);
-
+                    mov_count++;
                 }
+            }
+            //j now contains the index of the last element in the sorted list
+
+            if (low < j)
+            {
+                //move the pivot to its correct position in the list
+                swap(low, j);
+                mov_count++;
             }
         }
 
